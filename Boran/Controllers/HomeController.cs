e8fb66ff22eea48a2dfff4b6c212540a3f15a7ab
onurk79 <gezgin.779@hotmail.com>
2016-11-19 17:10:21 +0300

    finish

--- Boran/Controllers/HomeController.cs
+++ Boran/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Boran.Models;
+using Boran.ViewModels;
+using Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,23 +13,135 @@ namespace Boran.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private boranEntities context = new boranEntities();
+
+        public ActionResult Index(bool clearDb = false)
         {
-            return View();
+            if (clearDb)
+            {
+                context.db.RemoveRange(context.db.ToList());
+                context.SaveChanges();
+                return View();
+            }
+            else
+                return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(HttpPostedFileBase upload)
         {
-            ViewBag.Message = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                if (upload != null && upload.ContentLength > 0)
+                {
 
+                    Stream stream = upload.InputStream;
+
+                    IExcelDataReader reader = null;
+
+                    if (upload.FileName.EndsWith(".xlsx"))
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View();
+                    }
+
+                    reader.IsFirstRowAsColumnNames = true;
+
+                    DataSet result = reader.AsDataSet();
+                    reader.Close();
+                    foreach (DataRow Row in result.Tables[0].Rows)
+                    {
+                        Boran.Models.db db = new db();
+                        db.OrderID = Convert.ToInt32(Row[result.Tables[0].Columns[0]]);
+                        db.OrderDate = Convert.ToDateTime(Row[result.Tables[0].Columns[1]]);
+                        db.OrderPriority = Row[result.Tables[0].Columns[2]].ToString();
+                        db.OrderQuantity = Convert.ToInt32(Row[result.Tables[0].Columns[3]]);
+                        db.Discount = Convert.ToDouble(Row[result.Tables[0].Columns[4]]);
+                        db.ShipMode = Row[result.Tables[0].Columns[5]].ToString();
+                        db.UnitPrice = Convert.ToDouble(Row[result.Tables[0].Columns[6]]);
+                        db.UnitShippingCost = Convert.ToDouble(Row[result.Tables[0].Columns[7]]);
+                        db.CustomerName = Row[result.Tables[0].Columns[8]].ToString();
+                        db.Province = Row[result.Tables[0].Columns[9]].ToString();
+                        db.Region = Row[result.Tables[0].Columns[10]].ToString();
+                        db.CustomerSegment = Row[result.Tables[0].Columns[11]].ToString();
+                        db.ProductCategory = Row[result.Tables[0].Columns[12]].ToString();
+                        db.ProductSubCategory = Row[result.Tables[0].Columns[13]].ToString();
+                        db.ProductName = Row[result.Tables[0].Columns[14]].ToString();
+                        db.ProductContainer = Row[result.Tables[0].Columns[15]].ToString();
+                        if (Row[result.Tables[0].Columns[16]].ToString() == "")
+
+                            db.ProductBaseMargin = null;
+
+                        else
+
+                            db.ProductBaseMargin = Convert.ToDouble((Row[result.Tables[0].Columns[16]]));
+
+                        db.ShipDate = Convert.ToDateTime(Row[result.Tables[0].Columns[17]]);
+
+                        context.db.Add(db);
+
+                    }
+                    context.SaveChanges();
+
+                    return RedirectToAction("Calculation");
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+            }
             return View();
+
         }
+        List<DateTime> DateList = new List<DateTime>();
+        List<string> ProductCategoryList = new List<string>();
 
-        public ActionResult Contact()
+        public ActionResult Calculation()
         {
-            ViewBag.Message = "Your contact page.";
+            if (context.db.Count() == 0)
+            {
+                ModelState.AddModelError("File", "Database Null");
+                return View();
+            }
+            else
+            {
+                context.Database.CommandTimeout = 99000;
 
-            return View();
+                var date = (from a in context.db.OrderBy(x => x.OrderDate)
+                            group a by a.OrderDate.Value.Year into g
+                            select new
+                            {
+                                date = g.Key,
+                                count = g.Count(),
+
+                            }).ToList();
+                List<List<Sum>> sum = new List<List<Sum>>();
+                foreach (var item in date)
+                {
+                    List<Sum> result = (from a in context.db.Where(x => x.OrderDate.Value.Year == item.date)
+                                        group a by a.ProductCategory into g
+                                        select new Sum
+                                        {
+                                            Date = item.date,
+                                            ProductCategory = g.Key,
+                                            _totalPriceToCategory = g.Sum(oi => oi.OrderQuantity * (oi.UnitPrice - (oi.UnitPrice * oi.Discount) + oi.UnitShippingCost)),
+                                            _count = g.Count(),
+
+                                        }).ToList();
+                    foreach (var _string in result)
+                    {
+                        _string.TotalPriceToCategory = string.Format("{0:0,0.000}", _string._totalPriceToCategory);
+                        _string.Count = string.Format("{0:0,0.}", _string._count);
+                    }
+                    sum.Add(result);
+                }
+                return View(sum);
+            }
         }
     }
 }
---
+++ Boran/ViewModels/Sum.cs
@@ -0,0 +1,24 @@
+﻿namespace Boran.ViewModels
+{
+    public class Sum
+    {
+        public int Date { get; set;}
+        //public int OrderQuantity { get; set; }
+        //public double Discount { get; set; }
+        //public double UnitPrice { get; set; }
+        //public Double UnitShippingCost { get; set; }
+        public int _count { get; set; }
+        public string Count { get; set; }
+        public string ProductCategory { get; set; }
+        public double? _totalPriceToCategory { get; set; }
+        public string TotalPriceToCategory { get; set; }
+        //   public double price  { get { return price; } set { price = getPrice(); } }
+        // private double getPrice()
+        // {
+        //   return (OrderQuantity * (Convert.ToDouble(ShippingCost) - (Convert.ToDouble(ShippingCost) * Discount)));
+        // }
+    }
+     
+
+   
+}
